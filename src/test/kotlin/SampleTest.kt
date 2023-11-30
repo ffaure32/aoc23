10ea3722d8a1b34d73a6fa29d33e51aa36282f89
ffaure32 <frfaure32@gmail.com>
2023-11-30 22:45:32 +0100

    init project

---
+++ src/test/kotlin/SampleTest.kt
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class SampleTest {
+    @Test
+    fun add() {
+        val readInput = readInput(1)
+        readInput.forEach { println(it) }
+        assertEquals("ab", "ab")
+    }
+}
